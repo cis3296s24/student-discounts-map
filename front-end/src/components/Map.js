f584fdb10c9f19c1f32d457d08deeacca2ee327e
Christopher Christmas <cchristmas21@c8-89-f3-c3-8c-0a.wireless.temple.edu>
2024-04-10 16:38:13 -0400

    We will need to add the back-end instructions to the README. 1. npm install leaflet 2. yarn add leaflet 3. npm install @popperjs/core 4. yarn add @popperjs/core.

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,11 +1,28 @@
-import sampleMap from '../assets/img/sample-map.png'
+import React, { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-const Map = () => {
-    return (
-        <>
-            <img src={sampleMap} />
-        </>
-    )
-}
+const MapComponent = () => {
+  useEffect(() => {
+    // Ensure the div id 'map' is not already initialized by Leaflet
+    const container = L.DomUtil.get("map");
+    if (container != null) {
+      container._leaflet_id = null;
+    }
 
-export default Map
+    const map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap contributors",
+    }).addTo(map);
+
+    // Cleanup function to run when the component unmounts
+    return () => {
+      map.remove();
+    };
+  }, []); // The empty array ensures this effect runs only once upon mounting
+
+  return <div id="map" style={{ height: "500px" }}></div>;
+};
+
+export default MapComponent;
