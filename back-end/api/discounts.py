a044820892042b0c2924a7d9ba7515e97f87c271
kenichisutan <kenichi.jks@gmail.com>
2024-04-18 22:19:39 -0400

    Updated submission entry code

--- back-end/api/discounts.py
+++ back-end/api/discounts.py
@@ -1,12 +1,22 @@
+import datetime
+
 from flask import jsonify
 
 
 def submit(con, establishment_name, address, city, state, zip, discount, submitterID, lat, lng):
     cur = con.cursor()
 
-    query = "INSERT INTO Discounts (establishmentName, address, city, state, zip, discount, submitterID, lat, lng) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    lastUpdated = datetime.datetime.now()
 
-    cur.execute(query, (establishment_name, address, city, state, zip, discount, submitterID, lat, lng))
+    ## PLACE HOLDER VALUES
+    type = "restaurant"
+    validUntil = "2025-12-31"
+    rating = 5
+
+    query = ("INSERT INTO submissions (establishmentName, address, city, state, zip, discount, type, lastUpdated, validUntil, rating, lattitude, longitude, submitterID) "
+             "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+
+    cur.execute(query, (establishment_name, address, city, state, zip, discount, type, lastUpdated, validUntil, rating, lat, lng, submitterID))
 
     con.commit()
 
