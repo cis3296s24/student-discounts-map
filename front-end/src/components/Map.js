aeeb5c8800d7086d4ddcba52409d9d114a6ef8d6
kenichisutan <kenichi.jks@gmail.com>
2024-04-22 22:37:44 -0400

    Map switches focus when table is clicked

--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,77 +1,79 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
-  // declar variables
   const [tableData, setTableData] = useState([]);
+  const mapRef = useRef(null);
 
   useEffect(() => {
-    // Ensures the div id 'map' is not already initialized by Leaflet
     const container = L.DomUtil.get("map");
     if (container != null) {
       container._leaflet_id = null;
     }
 
     const map = L.map("map").setView([51.505, -0.09], 13);
+    mapRef.current = map;
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
     }).addTo(map);
 
-    // Function to add a marker to the map
     const addMarker = (e) => {
-      // Extract latitude and longitude from the event
       const { lat, lng } = e.latlng;
-
-      // Create and add the marker to the map
       L.marker([lat, lng]).addTo(map);
     };
 
-    // Add click event listener to the map
     map.on("click", addMarker);
 
-    // Cleanup function to run when the component unmounts
     return () => {
       map.remove();
     };
-  }, []); // The empty array ensures this effect runs only once upon mounting
+  }, []);
 
-  // load submissions from backend upon mounting
-    useEffect(() => {
-      async function retrieveData() {
-        const requestOptions = {
-          method: "GET",
-          credentials: "include",
-        };
+  const handleRowClick = (latitude, longitude) => {
+    console.log("Clicked row with latitude:", latitude, "longitude:", longitude);
 
-        const backendUrl = "http://localhost:5000/retrieve-all";
+    if (latitude !== undefined && longitude !== undefined && mapRef.current) {
+      const map = mapRef.current;
+      map.setView([latitude, longitude], 13);
+    } else {
+      console.error("Invalid latitude or longitude");
+    }
+  };
 
-        try {
-          const response = await fetch(backendUrl, requestOptions);
-          const data = await response.json();
+  useEffect(() => {
+    async function retrieveData() {
+      const requestOptions = {
+        method: "GET",
+        credentials: "include",
+      };
 
-          if (response.ok) {
-            // unpack JSON data
-            console.log(data);
-            setTableData(data);
-          } else {
-            console.log("Data fetch error:", data);
-          }
-        } catch (error) {
-          console.log("Error fetching user data:", error);
+      const backendUrl = "http://localhost:5000/retrieve-all";
+
+      try {
+        const response = await fetch(backendUrl, requestOptions);
+        const data = await response.json();
+
+        if (response.ok) {
+          console.log(data);
+          setTableData(data);
+        } else {
+          console.log("Data fetch error:", data);
         }
+      } catch (error) {
+        console.log("Error fetching user data:", error);
       }
+    }
 
-      retrieveData().then(() => {
-        console.log("Data retrieved");
-        //
-      });
-    }, []);
+    retrieveData().then(() => {
+      console.log("Data retrieved");
+    });
+  }, []);
 
   return (
       <div>
-        <div id="map" style={{height: "500px"}}></div>
+        <div id="map" style={{ height: "500px" }}></div>
         <table className="table">
           <thead>
           <tr>
@@ -87,28 +89,25 @@ const MapComponent = () => {
           </thead>
           <tbody>
           {tableData.map((data, index) => (
-              <tr key={index}>
+              <tr
+                  key={index}
+                  onClick={() => handleRowClick(data[11], data[12])}
+                  style={{ cursor: "pointer" }}
+              >
                 <th scope="row">{data[0]}</th>
-                {/* Using the first value in the inner array as the row number */}
                 <td>{data[1]}</td>
-                {/* Establishment Name */}
                 <td>{data[2]}</td>
-                {/* Address */}
                 <td>{data[3]}</td>
-                {/* City */}
                 <td>{data[4]}</td>
-                {/* State */}
                 <td>{data[5]}</td>
-                {/* Zip */}
                 <td>{data[6]}</td>
-                {/* Discount */}
                 <td>{data[13]}</td>
-                {/* Submitter ID */}
               </tr>
           ))}
           </tbody>
         </table>
-      </div>);
+      </div>
+  );
 };
 
 export default MapComponent;
