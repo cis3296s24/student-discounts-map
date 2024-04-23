595c3159af9eb81845d7f923727f98d043257162
kenichisutan <kenichi.jks@gmail.com>
2024-04-22 21:36:55 -0400

    Fixed submissionID error

--- front-end/src/components/Submit.js
+++ front-end/src/components/Submit.js
@@ -16,7 +16,7 @@ function SubmissionPage() {
     const [zip, setZip] = useState('');
     const [location, setLocation] = useState(null); 
     const [discount, setDiscount] = useState('');
-    const [submitterID, setSubmitterID] = useState('');
+    const [submitterID, setSubmitterID] = useState(1);
     const [lat, setLat] = useState(0);
     const [lng, setLng] = useState(0);
 
@@ -36,21 +36,20 @@ function SubmissionPage() {
         const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(fullAddress)}&key=${process.env.REACT_APP_OPENCAGE_API_KEY}`;
 
         console.log("Rendering Marker with:", establishmentName, discount);
-
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            if (data.results && data.results.length > 0) {
-                const { lat: tempLat, lng: tempLng } = data.results[0].geometry;
-                setLocation({ lat: tempLat, lng: tempLng }); // Update location state directly with the new values
-                console.log(tempLat, tempLng);
-            } else {
-                alert('Unable to geocode address.');
-            }
-        } catch (error) {
-            console.error('Geocoding error:', error);
-            alert('Error geocoding address. Please try again.');
-        }
+        
+        return fetch(apiUrl) // Return the fetch promise
+            .then(response => response.json())
+            .then(data => {
+                if (data.results && data.results.length > 0) {
+                    const { lat, lng } = data.results[0].geometry;
+                    setLocation({ lat, lng }); // Update location state
+                } else {
+                    alert('Unable to geocode address.');
+                }
+            }).catch(error => {
+                console.error('Geocoding error:', error);
+                alert('Error geocoding address. Please try again.');
+            });
     };
 
     // Function to handle form submission
@@ -62,12 +61,6 @@ function SubmissionPage() {
         geocodeAddress().then(() => {
             console.log("Geocoding complete.")
         });
-
-        // Check if the lat and lng are set
-        if (lat === 0 || lng === 0) {
-            alert('Please enter a valid address.');
-            return;
-        }
 
         // Check if the user is logged in
         if (jwtToken === '') {
